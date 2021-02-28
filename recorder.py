bc3e35cc16acc87e5880280e228177d4e6e2fcbc
vahidbe <vahid.beyraghi@student.uclouvain.be>
2021-02-28 16:50:40 +0100

    updating recorder for merging
    - Adding support for recordings without validation

--- recorder.py
+++ recorder.py
@@ -5,8 +5,9 @@ import os.path
 
 class Recorder:
 
-    def __init__(self, base_path):
+    def __init__(self, base_path, has_validation=False):
         self.base_path = base_path
+        self.has_validation = has_validation
         self.record = {'class_acc': [],
                        'loss_rpn_cls': [],
                        'loss_rpn_regr': [],
@@ -80,55 +81,56 @@ class Recorder:
             plt.show()
 
         # Validation
-        plt.figure(figsize=(15, 5))
-        plt.subplot(1, 2, 1)
-        plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_cls_val'], 'r')
-        plt.title('loss_rpn_cls_val')
-        plt.subplot(1, 2, 2)
-        plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_regr_val'], 'r')
-        plt.title('loss_rpn_regr_val')
-        if save:
-            plt.savefig(os.path.join(self.base_path, 'loss_rpn_cls_val&loss_rpn_regr_val.png'))
-        else:
-            plt.show()
+        if self.has_validation:
+            plt.figure(figsize=(15, 5))
+            plt.subplot(1, 2, 1)
+            plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_cls_val'], 'r')
+            plt.title('loss_rpn_cls_val')
+            plt.subplot(1, 2, 2)
+            plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_regr_val'], 'r')
+            plt.title('loss_rpn_regr_val')
+            if save:
+                plt.savefig(os.path.join(self.base_path, 'loss_rpn_cls_val&loss_rpn_regr_val.png'))
+            else:
+                plt.show()
 
-        plt.figure(figsize=(15, 5))
-        plt.subplot(1, 2, 1)
-        plt.plot(np.arange(0, num_epochs), self.record['loss_class_cls_val'], 'r')
-        plt.title('loss_class_cls_val')
-        plt.subplot(1, 2, 2)
-        plt.plot(np.arange(0, num_epochs), self.record['loss_class_regr_val'], 'r')
-        plt.title('loss_class_regr_val')
-        if save:
-            plt.savefig(os.path.join(self.base_path, 'loss_class_cls_val&loss_class_regr_val.png'))
-        else:
-            plt.show()
+            plt.figure(figsize=(15, 5))
+            plt.subplot(1, 2, 1)
+            plt.plot(np.arange(0, num_epochs), self.record['loss_class_cls_val'], 'r')
+            plt.title('loss_class_cls_val')
+            plt.subplot(1, 2, 2)
+            plt.plot(np.arange(0, num_epochs), self.record['loss_class_regr_val'], 'r')
+            plt.title('loss_class_regr_val')
+            if save:
+                plt.savefig(os.path.join(self.base_path, 'loss_class_cls_val&loss_class_regr_val.png'))
+            else:
+                plt.show()
 
-        plt.plot(np.arange(0, num_epochs), self.record['curr_loss_val'], 'r')
-        plt.title('total_loss_val')
-        if save:
-            plt.savefig(os.path.join(self.base_path, 'total_loss_val.png'))
-        else:
-            plt.show()
+            plt.plot(np.arange(0, num_epochs), self.record['curr_loss_val'], 'r')
+            plt.title('total_loss_val')
+            if save:
+                plt.savefig(os.path.join(self.base_path, 'total_loss_val.png'))
+            else:
+                plt.show()
 
-        plt.plot(np.arange(0, num_epochs), self.record['best_loss_val'], 'r')
-        plt.title('best_loss_val')
-        if save:
-            plt.savefig(os.path.join(self.base_path, 'best_loss_val.png'))
-        else:
-            plt.show()
+            plt.plot(np.arange(0, num_epochs), self.record['best_loss_val'], 'r')
+            plt.title('best_loss_val')
+            if save:
+                plt.savefig(os.path.join(self.base_path, 'best_loss_val.png'))
+            else:
+                plt.show()
 
-        plt.title('loss_val')
-        plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_cls_val'], 'b')
-        plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_regr_val'], 'g')
-        plt.plot(np.arange(0, num_epochs), self.record['loss_class_cls_val'], 'r')
-        plt.plot(np.arange(0, num_epochs), self.record['loss_class_regr_val'], 'c')
-        if save:
-            plt.savefig(os.path.join(self.base_path, 'loss_val.png'))
-        else:
-            plt.show()
+            plt.title('loss_val')
+            plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_cls_val'], 'b')
+            plt.plot(np.arange(0, num_epochs), self.record['loss_rpn_regr_val'], 'g')
+            plt.plot(np.arange(0, num_epochs), self.record['loss_class_cls_val'], 'r')
+            plt.plot(np.arange(0, num_epochs), self.record['loss_class_regr_val'], 'c')
+            if save:
+                plt.savefig(os.path.join(self.base_path, 'loss_val.png'))
+            else:
+                plt.show()
 
-    def add_new_entry(self, class_acc, loss_rpn_cls, loss_rpn_regr, loss_class_cls, loss_class_regr, curr_loss,
+    def add_new_entry_with_validation(self, class_acc, loss_rpn_cls, loss_rpn_regr, loss_class_cls, loss_class_regr, curr_loss,
                       elapsed_time, class_acc_val, loss_rpn_cls_val, loss_rpn_regr_val, loss_class_cls_val,
                       loss_class_regr_val, curr_loss_val, best_loss_val):
         self.record['class_acc'] += class_acc
@@ -145,6 +147,16 @@ class Recorder:
         self.record['class_acc_val'] += class_acc_val
         self.record['curr_loss_val'] += curr_loss_val
         self.record['best_loss_val'] += best_loss_val
+
+    def add_new_entry(self, class_acc, loss_rpn_cls, loss_rpn_regr, loss_class_cls, loss_class_regr, curr_loss,
+                      elapsed_time):
+        self.record['class_acc'] += class_acc
+        self.record['loss_rpn_cls'] += loss_rpn_cls
+        self.record['loss_rpn_regr'] += loss_rpn_regr
+        self.record['loss_class_cls'] += loss_class_cls
+        self.record['loss_class_regr'] += loss_class_regr
+        self.record['curr_loss'] += curr_loss
+        self.record['elapsed_time'] += elapsed_time
 
     def show_graphs(self):
         self._create_graphs(False)
