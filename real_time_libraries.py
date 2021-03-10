95ae9babc074cd7733df96176d3424f80fa3775e
vahidbe <vahid.beyraghi@student.uclouvain.be>
2021-03-10 16:35:15 +0100

    Start of the python trap script
    - Demo added and can be launched with parameter --demo=True with the raspberry_real_time script
    - Start of the trap behavior

---
+++ real_time_libraries.py
@@ -0,0 +1,199 @@
+import imutils
+
+from libraries import *
+from imutils.video import VideoStream
+from imutils.video import FPS
+from datetime import datetime
+
+
+def init_models(C):
+    num_features = 512
+
+    input_shape_img = (None, None, 3)
+    input_shape_features = (None, None, num_features)
+
+    img_input = Input(shape=input_shape_img)
+    roi_input = Input(shape=(C.num_rois, 4))
+    feature_map_input = Input(shape=input_shape_features)
+
+    # define the base network (VGG here, can be Resnet50, Inception, etc)
+    shared_layers = nn_base(img_input, trainable=True)
+
+    # define the RPN, built on the base layers
+    num_anchors = len(C.anchor_box_scales) * len(C.anchor_box_ratios)
+    rpn_layers = rpn_layer(shared_layers, num_anchors)
+
+    classifier = classifier_layer(feature_map_input, roi_input, C.num_rois, nb_classes=len(C.class_mapping))
+
+    model_rpn = Model(img_input, rpn_layers)
+    model_classifier_only = Model([feature_map_input, roi_input], classifier)
+
+    model_classifier = Model([feature_map_input, roi_input], classifier)
+
+    print('Loading weights from {}'.format(C.model_path))
+    model_rpn.load_weights(C.model_path, by_name=True)
+    model_classifier.load_weights(C.model_path, by_name=True)
+
+    model_rpn.compile(optimizer='sgd', loss='mse')
+    model_classifier.compile(optimizer='sgd', loss='mse')
+
+    class_mapping = C.class_mapping
+    class_mapping = {v: k for k, v in class_mapping.items()}
+    print(class_mapping)
+
+    return model_rpn, class_mapping, model_classifier_only
+
+
+def detect(img, model_rpn, model_classifier_only, C, class_mapping, bbox_threshold, class_to_color):
+    st = time.time()
+    X, ratio = format_img(img, C)
+
+    X = np.transpose(X, (0, 2, 3, 1))
+
+    # get output layer Y1, Y2 from the RPN and the feature maps F
+    # Y1: y_rpn_cls
+    # Y2: y_rpn_regr
+    [Y1, Y2, F] = model_rpn.predict(X)
+
+    # Get bboxes by applying NMS
+    # R.shape = (300, 4)
+    R = rpn_to_roi(Y1, Y2, C, K.image_data_format(), overlap_thresh=0.7)
+
+    # convert from (x1,y1,x2,y2) to (x,y,w,h)
+    R[:, 2] -= R[:, 0]
+    R[:, 3] -= R[:, 1]
+
+    # apply the spatial pyramid pooling to the proposed regions
+    bboxes = {}
+    probs = {}
+
+    for jk in range(R.shape[0] // C.num_rois + 1):
+        ROIs = np.expand_dims(R[C.num_rois * jk:C.num_rois * (jk + 1), :], axis=0)
+        if ROIs.shape[1] == 0:
+            break
+
+        if jk == R.shape[0] // C.num_rois:
+            # pad R
+            curr_shape = ROIs.shape
+            target_shape = (curr_shape[0], C.num_rois, curr_shape[2])
+            ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+            ROIs_padded[:, :curr_shape[1], :] = ROIs
+            ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
+            ROIs = ROIs_padded
+
+        [P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
+
+        # Calculate bboxes coordinates on resized image
+        for ii in range(P_cls.shape[1]):
+            # Ignore 'bg' class
+            if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
+                continue
+
+            cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
+
+            if cls_name not in bboxes:
+                bboxes[cls_name] = []
+                probs[cls_name] = []
+
+            (x, y, w, h) = ROIs[0, ii, :]
+
+            cls_num = np.argmax(P_cls[0, ii, :])
+            try:
+                (tx, ty, tw, th) = P_regr[0, ii, 4 * cls_num:4 * (cls_num + 1)]
+                tx /= C.classifier_regr_std[0]
+                ty /= C.classifier_regr_std[1]
+                tw /= C.classifier_regr_std[2]
+                th /= C.classifier_regr_std[3]
+                x, y, w, h = apply_regr(x, y, w, h, tx, ty, tw, th)
+            except:
+                pass
+            bboxes[cls_name].append(
+                [C.rpn_stride * x, C.rpn_stride * y, C.rpn_stride * (x + w), C.rpn_stride * (y + h)])
+            probs[cls_name].append(np.max(P_cls[0, ii, :]))
+
+    all_dets = []
+
+    for key in bboxes:
+        bbox = np.array(bboxes[key])
+
+        new_boxes, new_probs = non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.2)
+        for jk in range(new_boxes.shape[0]):
+            (x1, y1, x2, y2) = new_boxes[jk, :]
+
+            # Calculate real coordinates on original image
+            (real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
+            cv2.rectangle(img, (real_x1, real_y1), (real_x2, real_y2),
+                          (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),
+                          4)
+
+            textLabel = '{}: {}'.format(key, int(100 * new_probs[jk]))
+            all_dets.append((key, 100 * new_probs[jk], ((real_x1, real_y1), (real_x2, real_y2))))
+
+            (retval, baseLine) = cv2.getTextSize(textLabel, cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+            textOrg = (real_x1, real_y1 - 0)
+
+            cv2.rectangle(img, (textOrg[0] - 5, textOrg[1] + baseLine - 5),
+                          (textOrg[0] + retval[0] + 5, textOrg[1] - retval[1] - 5), (0, 0, 0), 1)
+            cv2.rectangle(img, (textOrg[0] - 5, textOrg[1] + baseLine - 5),
+                          (textOrg[0] + retval[0] + 5, textOrg[1] - retval[1] - 5), (255, 255, 255), -1)
+            cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+
+    if C.verbose:
+        print(all_dets[:1])
+        print('Elapsed time = {}'.format(time.time() - st))
+
+    return all_dets
+
+
+def get_detections(model_rpn, model_classifier_only, C, class_mapping, bbox_threshold, class_to_color):
+    # initialize the video stream, allow the camera sensor to warmup,
+    # and initialize the FPS counter
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(1.0)
+    try:
+        while True:
+            img = vs.read()
+            imutils.resize(img, width=400)
+
+            all_dets = detect(img, model_rpn, model_classifier_only, C, class_mapping, bbox_threshold, class_to_color)
+            if not len(all_dets) == 0:
+                for detection, probability, bbox in all_dets:
+                    yield detection, probability, bbox
+    except KeyboardInterrupt:
+        print("[INFO] ending video stream...")
+        vs.stop()
+
+
+def run_demo(model_rpn, model_classifier_only, C, class_mapping, bbox_threshold, class_to_color):
+    # initialize the video stream, allow the camera sensor to warmup,
+    # and initialize the FPS counter
+    print("[INFO] starting video stream...")
+    vs = VideoStream(src=0).start()
+    time.sleep(1.0)
+    fps = FPS().start()
+    while True:
+        img = vs.read()
+        imutils.resize(img, width=400)
+
+        detect(img, model_rpn, model_classifier_only, C, class_mapping, bbox_threshold, class_to_color)
+        cv2.imshow("Frame", img)
+
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+        # update the FPS counter
+        fps.update()
+
+    fps.stop()
+    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    print("[INFO] ending video stream...")
+    vs.stop()
+
+
+def get_timestamp():
+    return datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
