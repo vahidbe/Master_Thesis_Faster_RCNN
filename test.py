1392d84efd4b520e61a34cb1842253abe544941d
Jonathan Thibaut <jothibautucl@github.com>
2021-03-11 15:42:00 +0100

    Real time detection runs on RPI

--- test.py
+++ test.py
@@ -1,16 +1,25 @@
-import pandas as pd
-import ast
-
-
-if __name__ == '__main__':
-    imgs_record_df = pd.read_csv('./logs/model6000_val_epoch - imgs.csv')
-    last_row = imgs_record_df.tail(1)
-    test_imgs_temp = ast.literal_eval(last_row['train'].tolist()[0])
-    test_imgs = []
-    for img_dict in test_imgs_temp:
-        print(img_dict)
-        # print(img_dict['bboxes'][0]['class'])
-        test_imgs.append(img_dict['filepath'])
-
-    # print(test_imgs)
-
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+# allow the camera to warmup
+time.sleep(0.1)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+            break
