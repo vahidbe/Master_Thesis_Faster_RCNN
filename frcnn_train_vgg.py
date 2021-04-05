334443bd6b6dea3b87ebf74bfc3beec575148d7e
jothibautucl <44204282+jothibautucl@users.noreply.github.com>
2021-04-05 02:16:53 +0200

    Partial results for validation of gamma

--- frcnn_train_vgg.py
+++ frcnn_train_vgg.py
@@ -544,15 +544,29 @@ if __name__ == "__main__":
 
     import itertools as it
 
+    # param = {
+    #     'histogram_equalization': [True, False],
+    #     'noise_reduction': [None, "box_filter", "gaussian"],
+    #     'gamma_correction': [True, False],
+    #     'brightness_jitter': [True, False]
+    # }
+
     param = {
-        'histogram_equalization': [True, False],
-        'noise_reduction': [None, "box_filter", "gaussian"],
+        'brightness_jitter': [True, False],
+        'brightness_jitter_bound': [0.1],
         'gamma_correction': [True, False],
-        'brightness_jitter': [True, False]
+        'gamma_value': [1.5]
     }
 
+    combinations = [
+        (True, 0.1, False, 1.0),
+        (True, 0.2, False, 1.0),
+        (True, 0.3, False, 1.0),
+        (True, 0.4, False, 1.0)
+    ]
+
     paramNames = list(param.keys())
-    combinations = it.product(*(param[Name] for Name in paramNames))
+    # combinations = it.product(*(param[Name] for Name in paramNames))
 
     model_all, model_rpn, model_classifier = initialize_model()
     initial_weights_rpn = model_rpn.get_weights()
